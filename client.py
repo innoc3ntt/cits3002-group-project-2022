8a2497dd39221ff39c095f6f13e9d88363edfd78
tim <timothy.tan.93@gmail.com>
2022-05-02 07:51:17 +1000

    parsing works

--- client.py
+++ client.py
@@ -1,37 +1,44 @@
-import os, re
+import re
 
-global_vars = []
-action_set = []
+configs = []
+actions = []
 buffer = []
 
 with open("rakefile") as file:
     for line in file:
         chars = line.split()
-        # If line is a global variable
+
         if "=" in chars:
-            # Check if each option has an additional port given with :
+            # If line is a global variable
+
             temp = []
             for word in chars[2:]:
+                # Check if each option has an additional port given with :
                 if ":" in word:
                     temp.append(word.split(":"))
                 else:
                     temp.append(word)
-            global_vars.append({chars[0]: temp})
+            configs.append({chars[0]: temp})
 
-        if re.match(r"^.*\:$", line):
-            if len(action_set) > 0:
-                action_set[-1].append(buffer)
-            action_set.append([line.split()[0]])
-            # clear the buffer
+        elif re.match(r"^.*\:$", line):
+            # if line defines an actionset
+            if len(actions):
+                # if actions is not empty
+                actions[-1].append(buffer)
+
+            # create a new actionset and clear the buffer
+            actions.append([line.split()[0]])
             buffer = []
 
-        if re.match(r"^\s{4}[a-z]", line):
+        elif re.match(r"^\s{4}[a-z]", line):
+            # if line begins with 4 spaces/1 tab, defines a command
             buffer.append(line.split())
 
-        if re.match(r"^\s{8}[a-z]", line):
-            # if this is first line of command
+        elif re.match(r"^\s{8}[a-z]", line):
+            # if line beings with 8 spaces/2 tabs, defines additional arguments
             buffer[-1].append(line.split())
 
-    action_set[-1].append(buffer)
+    # on last line of file, add the buffer to last action set
+    actions[-1].append(buffer)
 
-print(action_set)
+print(actions)
