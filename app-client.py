5b49a2bca76500682b98749200c27dc20f0eaabf
tim <timothy.tan.93@gmail.com>
2022-05-11 16:17:15 +0800

    add source files

---
+++ app-client.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+
+import sys
+import socket
+import selectors
+import traceback
+
+import libclient
+
+sel = selectors.DefaultSelector()
+
+
+def create_request(action, value, subp="echo"):
+    if action == "search":
+        return dict(
+            type="text/json",
+            encoding="utf-8",
+            content=dict(action=action, value=value),
+        )
+    elif action == "query":
+        return dict(
+            type="text/json",
+            encoding="utf-8",
+            content=dict(action=action),
+        )
+    elif action == "remote":
+        return dict(
+            type="text/json",
+            encoding="utf-8",
+            content=dict(action=action, shell=subp, value=value),
+        )
+    else:
+        return dict(
+            type="binary/custom-client-binary-type",
+            encoding="binary",
+            content=bytes(action + value, encoding="utf-8"),
+        )
+
+
+def start_connection(host, port, request):
+    addr = (host, port)
+    print(f"Starting connection to {addr}")
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setblocking(False)
+    sock.connect_ex(addr)
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    message = libclient.Message(sel, sock, addr, request)
+    sel.register(sock, events, data=message)
+
+
+host = "127.0.0.1"
+action = "query"
+
+
+# if len(sys.argv) != 5:
+#     print(f"Usage: {sys.argv[0]} <host> <port> <action> <value>")
+#     sys.exit(1)
+
+# host, port = sys.argv[1], int(sys.argv[2])
+# action, value = sys.argv[3], sys.argv[4]
+request = create_request(action, 1)
+
+hosts = [65432]
+
+results = []
+
+
+for x in hosts:
+    start_connection(host, x, request)
+
+try:
+    while True:
+        events = sel.select(timeout=1)
+        for key, mask in events:
+            message = key.data
+            try:
+                message.process_events(mask)
+                print(key.data)
+                if mask & selectors.EVENT_READ:
+                    results.append(
+                        {
+                            "address": message.addr,
+                            "cost": message.response["result"],
+                        }
+                    )
+            except Exception:
+                print(
+                    f"Main: Error: Exception for {message.addr}:\n"
+                    f"{traceback.format_exc()}"
+                )
+                message.close()
+        # Check for a socket being monitored to continue.
+        if not sel.get_map():
+            break
+
+    minCost = min(results, key=lambda x: x["cost"])
+    print(minCost)
+
+    host, port = minCost["address"]
+    request = create_request("remote", "-l", subp="ls")
+
+    start_connection(host, port, request)
+
+    while True:
+        events = sel.select(timeout=1)
+        for key, mask in events:
+            message = key.data
+            try:
+                message.process_events(mask)
+            except Exception:
+                print(
+                    f"Main: Error: Exception for {message.addr}:\n"
+                    f"{traceback.format_exc()}"
+                )
+                message.close()
+        # Check for a socket being monitored to continue.
+        if not sel.get_map():
+            break
+
+
+except KeyboardInterrupt:
+    print("Caught keyboard interrupt, exiting")
+finally:
+    sel.close()
