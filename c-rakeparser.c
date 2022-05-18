33145dceb308297bbadddfe768882c8f325d0246
Dougal4Hamish <gillespiesafe@gmail.com>
2022-05-18 13:28:19 +0800

    parsing allmost finished

--- c-rakeparser.c
+++ c-rakeparser.c
@@ -3,13 +3,14 @@
 #include  <stdlib.h>
 #include  <unistd.h>
 #include <string.h>
-// #include <"strsplit.c">
+#include <stdbool.h>
+
 
 extern  char    **strsplit(const char *line, int *nwords);
 extern  void    free_words(char **words);
 
 #define     BUFSIZE      10000
-#define     DICTIONARY  "/Users/hamishgillespie/Desktop/netWORKS/project/cits3002-group-project-2022/rakefile1"
+#define     DICTIONARY  "/Users/hamishgillespie/Desktop/netWORKS/project/cits3002-group-project-2022/rakefile3"
 #define     MAX_HOSTNAME_LEN    20
 
 
@@ -36,6 +37,7 @@ int main(int argc, char const *argv[])
 
 
     //procesing each line of the file
+    printf("Inital File read----------------------------\n");
     while( fgets(line, sizeof line, dict) != NULL ) {  
         // printing each line in rakefile for debugging purposes
         printf("%s", line);
@@ -43,18 +45,19 @@ int main(int argc, char const *argv[])
         // counting the number of actionsets in the rakefile
         if( strstr(line,"actionset") ){
             act_set_count++;
-            if( strstr(line,"   ") ){
+            //if line contains actionset but also contains a tab, we dont want to count it twice
+            if( strstr(line,"    ") ){
                 act_set_count--;
             }
         }
 
         //if the line contains a port
         if( strstr(line,"PORT") ){
-            printf("we got a config port line: %s", line);
+            // printf("we got a config port line: %s", line);
             int nwords;
             char **words = strsplit(line, &nwords);
             for(int w=0 ; w<nwords ; ++w) {
-                printf("\t[%i]  \"%s\"\n", w, words[w]);
+                // printf("\t[%i]  \"%s\"\n", w, words[w]);
             }
             PORT = atoi(words[2]);
         }
@@ -64,7 +67,7 @@ int main(int argc, char const *argv[])
             int nwords;
             char **words = strsplit(line, &nwords);
             for(int w=0 ; w<nwords ; ++w) {
-                printf("\t[%i]  \"%s\"\n", w, words[w]);
+                // printf("\t[%i]  \"%s\"\n", w, words[w]);
             }
             for(int i =0; i < nwords -2 ; ++i){
                 HOSTS[i] = words[i+2];
@@ -73,15 +76,63 @@ int main(int argc, char const *argv[])
         }
 
     }
-    printf("\n");
-    printf("the port is %i\n", PORT);
-    printf("these are the hosts:\n");
-    for(int i =0; i < NUM_HOSTS; i++){
-        printf("\t%s\n", HOSTS[i]);
+    fclose(dict);
+    printf("\nEnd of first opening of rakeFile------------\n");
+
+
+    //procesing each actionset of file--------------------------
+    dict = fopen(DICTIONARY, "r");
+
+    if(dict == NULL) {
+        printf( "cannot open dictionary '%s'\n", DICTIONARY);
+        exit(EXIT_FAILURE);
     }
 
+    //Creating array to hold the amount of actions in each actionset
+    int action_counts[act_set_count];
+    //setting all actionset counts to 0;
+    for (size_t i = 0; i < act_set_count; i++)
+    {
+        action_counts[i] = 0;
+    }
+    //a variable to track what action set we are in (i.e. actionset1, actionset2 ...)
+    int cur_act_set = -1;
+    // a variabl to track the action count in the current action set
 
-    // printf("%i\n\n", act_set_count);
+    
+
+    while( fgets(line, sizeof line, dict) != NULL ) {
+
+        if(strstr(line,"#")) continue;
+
+        //entering a new actions set
+        if( strstr(line,"actionset") && !strstr(line,"    ") ){
+            cur_act_set++;
+            // printf("swag\n");
+        }
+
+        //action line, and not a required line
+        if(strstr(line,"    ") && !strstr(line,"        ")){
+            action_counts[cur_act_set]++;
+        }
+
+
+
+    } 
     fclose(dict);
+
+
+
+    printf("\n\nFILE SUMMARY------------\n");
+    printf("the port is %i\n", PORT);
+    printf("these are the hosts:");
+    for(int i =0; i < NUM_HOSTS; i++){
+        printf("\n\t%s", HOSTS[i]);
+    }
+    printf("\nThere are %i action sets\n", act_set_count);
+    //iterating through action_counts;
+    for(int i = 0; i < act_set_count; i++){
+        printf("actionset: %i has %i actions\n", i ,action_counts[i]);
+    }
     return 0;
 }
